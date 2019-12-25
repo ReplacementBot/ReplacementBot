7ab7a4630272a6475c161c0d8209cef316eb62b0
MrBartusek <bartusekcraft@gmail.com>
2019-12-25 18:23:52 +0100

    removed config auto set test

--- tests/configuration.test.js
+++ tests/configuration.test.js
@@ -1,14 +1,5 @@
 const { Config, ConfigSettings, ConfigSources } = require('../src/managers/config');
 
-describe('Configuration Settings', () =>
-{
-	test('should set to file if auto', () =>
-	{
-		const settings = new ConfigSettings(ConfigSources.AUTO);
-		expect(settings.source).toBe(ConfigSources.FILE);
-	});
-});
-
 describe('Configuration', () =>
 {
 	test('should load from file', () =>
