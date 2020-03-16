72e2f20b17bd7668b56723f9068dbef462c8e475
MrBartusek <bartusekcraft@gmail.com>
2020-03-16 16:47:54 +0100

    Fixed `vulcanFetcher` tests

--- tests/vulcanFetcher.test.ts
+++ tests/vulcanFetcher.test.ts
@@ -5,14 +5,16 @@ import Replacement from '../src/models/replacement';
 import Lesson from '../src/models/lesson';
 import Teacher from '../src/models/teacher';
 import { FetchError } from '../src/models/replacementsFetcher';
+import moment from 'moment';
 
 describe('Vulcan Fetcher', () =>
 {
+	const fetchingTime = moment();
 	test('should fetch replacements', async () =>
 	{
 		new Config(new ConfigSettings(ConfigSources.FILE, 'tests/resources/vulcanFetcherTest.yaml')).makeStatic();
-		await expect(new VulcanFetcher().fetchReplacements()).resolves.toStrictEqual(
-			new ReplacementDay(undefined, [
+		await expect(new VulcanFetcher().fetchReplacements(fetchingTime)).resolves.toStrictEqual(
+			new ReplacementDay(fetchingTime, [
 				new Replacement(new Lesson(1), 'Description1', new Teacher('AbsentTeacher1'), new Teacher('NewTeacher1'), 'Comment1'),
 				new Replacement(new Lesson(2), 'Description2', new Teacher('AbsentTeacher2'), new Teacher('NewTeacher2'), 'Comment2'),
 			]),
@@ -22,14 +24,14 @@ describe('Vulcan Fetcher', () =>
 	test('should give empty result', async () =>
 	{
 		new Config(new ConfigSettings(ConfigSources.FILE, 'tests/resources/vulcanFetcherEmptyTest.yaml')).makeStatic();
-		await expect(new VulcanFetcher().fetchReplacements()).resolves.toStrictEqual(
-			new ReplacementDay(undefined, []),
+		await expect(new VulcanFetcher().fetchReplacements(moment(fetchingTime))).resolves.toStrictEqual(
+			new ReplacementDay(fetchingTime, []),
 		);
 	});
 
 	test('should handle errors', async () =>
 	{
 		new Config(new ConfigSettings(ConfigSources.FILE, 'tests/resources/vulcanFetcherErrorTest.yaml')).makeStatic();
-		await expect(new VulcanFetcher().fetchReplacements()).rejects.toEqual(new FetchError('Server returned bad code (500)'));
+		await expect(new VulcanFetcher().fetchReplacements(moment())).rejects.toEqual(new FetchError('Server returned bad code (500)'));
 	});
 });
