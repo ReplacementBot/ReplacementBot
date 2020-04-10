4ebff7a208c200718c077a759c84cfd38bd1f839
MrBartusek <bartusekcraft@gmail.com>
2020-04-10 20:51:04 +0200

    ReplacementManager test

---
+++ tests/managers/replacementsManager.test.ts
@@ -0,0 +1,26 @@
+import ReplacementsManager from '../../src/managers/replacementsManager';
+import ReplacementDay from '../../src/models/replacementDay';
+import moment from 'moment';
+import Replacement from '../../src/models/replacement';
+import Lesson from '../../src/models/lesson';
+import Config from '../../src/managers/config';
+
+describe('ReplacementManager', () =>
+{
+	describe('Should Fetch', async () =>
+	{
+		Config.initialize();
+		const manager = new ReplacementsManager();
+		const fetchingTime = moment();
+		test('Should Initialize', () =>
+		{
+			return expect(manager.initialize('dummyFetcher')).resolves.toBe(undefined);
+		});
+		test('Should Fetch', () =>
+		{
+			return expect(manager.fetchReplacements(fetchingTime)).resolves.toStrictEqual(
+				new ReplacementDay(fetchingTime, [ new Replacement(new Lesson(0, 'Dummy Lesson'), 'Dummy')]),
+			);
+		});
+	});
+});
