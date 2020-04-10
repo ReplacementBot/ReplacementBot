fd21b085c5837f75b81504eeef0385b209be6cf7
MrBartusek <bartusekcraft@gmail.com>
2020-04-10 20:21:39 +0200

    Removed ability to mute logger (was needed for removed system tests)

--- src/managers/logger.ts
+++ src/managers/logger.ts
@@ -4,8 +4,6 @@ import TestUtilities from '../../tests/util';
 
 export default class Logger
 {
-	private static muted = false;
-
 	public static fatalAndCrash(message: string, exitCode?: number): void
 	{
 		if(exitCode == undefined)
@@ -24,35 +22,24 @@ export default class Logger
 	}
 	public static fatal(message: string): void
 	{
-		if(Logger.muted) return;
 		console.log(chalk.bold.red('[FATAL ERROR] ') + chalk.redBright(message));
 	}
 	public static error(message: string): void
 	{
-		if(Logger.muted) return;
 		console.log(chalk.bold.red('[ERROR] ') + chalk.redBright(message));
 	}
 	public static warn(message: string): void
 	{
-		if(Logger.muted) return;
 		console.log(chalk.bold.yellow('[WARN] ') + chalk.yellowBright(message));
 	}
 	public static info(message: string): void
 	{
-		if(Logger.muted) return;
 		console.log(chalk.bold.white('[INFO] ') + message);
 	}
 	public static printLogo(): void
 	{
-		if(Logger.muted) return;
 		console.log();
 		console.log(chalk.magenta(figlet.textSync('Replacement Bot')));
 		console.log();
-	}
-
-	public static mute(): void
-	{
-		console.log(chalk.green('Logger has been muted'));
-		Logger.muted = true;
 	}
 }
