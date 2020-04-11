f150878902b496f1fa431cd57e62e4e31d46c05e
MrBartusek <bartusekcraft@gmail.com>
2020-04-11 15:11:41 +0200

    tweeked `WebFetcher` test timeouts

--- tests/util/webFetcher.test.ts
+++ tests/util/webFetcher.test.ts
@@ -11,19 +11,19 @@ describe('WebFetcher', () =>
 				result: expect.stringContaining('Table playground'),
 			}),
 		);
-	});
+	}, 10000);
 
 	test('should fail on 404', async () =>
 	{
 		await expect(new WebFetcher().request('https://httpstat.us/404', 'UTF-8')).rejects.toStrictEqual(
 			new HTTPResponse(HTTPResponseType.BAD_CODE, undefined, 404),
 		);
-	});
+	}, 10000);
 
 	test('should throw error on invalid encoding argument', async () =>
 	{
 		await expect(new WebFetcher().request(TEST_URL, 'Lorem ipsum')).rejects.toEqual(
 			new HTTPResponse(HTTPResponseType.FAILED, 'WebFetcher Error: "Lorem ipsum" encoding don\'t exist'),
 		);
-	});
+	}, 10000);
 });
