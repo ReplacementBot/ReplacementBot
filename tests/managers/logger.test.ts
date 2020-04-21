c7b079bdb8cef1140dc7747591ff91d494e10709
MrBartusek <bartusekcraft@gmail.com>
2020-04-22 00:17:14 +0200

    'tips' logger test

--- tests/managers/logger.test.ts
+++ tests/managers/logger.test.ts
@@ -8,11 +8,22 @@ describe('Logger', () =>
 		Logger.info('Test');
 		expect(console.log).toHaveBeenCalledWith(chalk.bold.white('[INFO] ') + chalk.white('Test'));
 	});
+
 	test('should crash on fatal', () =>
 	{
 		expect(() =>
 		{
 			Logger.critical('Test');
 		}).toThrow();
+	});
+
+	test('should give tips', () =>
+	{
+		console.log = jest.fn();
+		expect(() =>
+		{
+			Logger.critical('REPLACEMENT_BOT_TOKEN is not provided');
+		}).toThrow();
+		expect(console.log).toHaveBeenCalledWith(chalk.green('TIP: You are missing bot token, please read "Configure Bot" article on wiki!'));
 	});
 });
