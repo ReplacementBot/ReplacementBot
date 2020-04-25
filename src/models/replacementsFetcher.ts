6630442a1a4b5c2fe103543cffed5b50418d8cd9
MrBartusek <bartusekcraft@gmail.com>
2020-04-25 19:03:13 +0200

    Moved errors to separate file

--- src/models/replacementsFetcher.ts
+++ src/models/replacementsFetcher.ts
@@ -1,23 +1,8 @@
 import ReplacementDay from './replacementDay';
 import moment = require('moment');
 
-export class FetchError extends Error
-{
-	constructor(message: any)
-	{
-		super(message);
-	}
-}
-
-export class ResponseParseError extends Error
-{
-	constructor(message: any)
-	{
-		super(message);
-	}
-}
-
 export interface ReplacementsFetcher
 {
-	fetchReplacements(date: moment.Moment): Promise<ReplacementDay | FetchError | ResponseParseError>;
+	initialize?(config: any): Promise<void>;
+	fetchReplacements(date: moment.Moment): Promise<ReplacementDay>;
 }
---
+++ src/util/errors.ts
@@ -0,0 +1,23 @@
+export class FetchError extends Error
+{
+	constructor(message: any)
+	{
+		super(message);
+	}
+}
+
+export class ResponseParseError extends Error
+{
+	constructor(message: any)
+	{
+		super(message);
+	}
+}
+
+export class FetcherResultError extends Error
+{
+	constructor(message: any)
+	{
+		super(message);
+	}
+}
