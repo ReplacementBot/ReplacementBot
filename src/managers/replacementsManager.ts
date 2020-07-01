2e80837d6a0dab24339a46cc4a1257edcb7f117d
MrBartusek <bartusekcraft@gmail.com>
2020-07-01 17:18:42 +0200

    Spearate `ReplacementsManager` initialization to other class

--- src/managers/replacementsManager.ts
+++ src/managers/replacementsManager.ts
@@ -1,97 +1,22 @@
 import { ReplacementsFetcher } from '../models/replacementsFetcher';
 import ReplacementDay from '../models/replacementDay';
 import Config from './config';
-import fs from 'fs';
-
 import FetcherMetadata from '../models/fetcherMetadata';
 import moment, { Moment } from 'moment';
-import TestUtilities from '../../tests/util';
+import FetcherLoader from '../util/fetcherLoader';
 
 export default class ReplacementsManager
 {
 	fetcher: ReplacementsFetcher;
 	metadata: FetcherMetadata;
 
-	// INITIALIZATION
-
 	public async initialize(fetcherName: string): Promise<string>
 	{
-		if(this.fetcher !== undefined)
-		{
-			throw new Error('ReplacementsManager cannot be initialized twice');
-		}
-
-		await this.loadFetcher(fetcherName);
-		this.metadata = this.loadMetadata(fetcherName);
-		return fetcherName;
+		const result = await FetcherLoader.loadFetcher(fetcherName);
+		this.fetcher = result[0];
+		this.metadata = result[1];
+		return this.metadata.getName();
 	}
-
-	private loadFetcher(fetcherName: string): Promise<void>
-	{
-		return new Promise((resolve, reject) =>
-		{
-			// While running bot it doesn't work with ts files anymore
-			// Hoverer, Jest don't create js files and still operates on ts
-			const path = `../fetchers/${fetcherName}/fetcher.${TestUtilities.isRunningInTest() ? 'ts' : 'js'}`;
-
-			import(path)
-				.then((fetcherClass) =>
-				{
-					if(!this.isFetcher(fetcherClass))
-					{
-						return Promise.reject(new Error(`"${fetcherName}" is not a ReplacementsFetcher`));
-					}
-
-					try
-					{
-						// fetcher constructor
-						this.fetcher = new fetcherClass.default();
-					}
-					catch
-					{
-						reject(new Error('fetcher thrown error inside constructor'));
-						return;
-					}
-
-					if(this.fetcher.initialize)
-					{
-						this.fetcher.initialize(Config.get('fetcher').config)
-							.then(() =>
-							{
-								resolve();
-							})
-							.catch((error) =>
-							{
-								reject(new Error(`fetcher initialization error (${error})`));
-							});
-					}
-					else
-					{
-						resolve();
-					}
-
-				})
-				.catch((error) =>
-				{
-					reject(new Error(`Failed to import fetcher "${fetcherName}" (${error})`));
-				});
-		});
-	}
-
-	private loadMetadata(fetcherName: string): FetcherMetadata
-	{
-		const data = fs.readFileSync(__dirname + `/../fetchers/${fetcherName}/metadata.json`);
-		return new FetcherMetadata(data.toString(), fetcherName);
-	}
-
-	private isFetcher(object: any): boolean
-	{
-		// fetcher constructor
-		const testObject = new object.default();
-		return testObject.fetchReplacements != undefined;
-	}
-
-	// RETRIEVING DATA
 
 	public getMetadata(): FetcherMetadata
 	{
@@ -130,21 +55,11 @@ export default class ReplacementsManager
 		return result;
 	}
 
-	// HELPERS
-
 	private getDefaultDate(): Moment
 	{
 		const switchHour = Config.get('daySwitchHour');
-		if(!switchHour)
-		{
-			return moment();
-		}
 		const switchHourMoment = moment(switchHour, 'k-m');
-		if(!switchHourMoment.isValid())
-		{
-			return moment();
-		}
-		if(moment().diff(switchHourMoment, 'minutes') >= 0)
+		if(switchHourMoment.isValid() && moment().diff(switchHourMoment, 'minutes') >= 0)
 		{
 			return moment().add(1, 'days');
 		}
---
+++ src/util/fetcherLoader.ts
@@ -0,0 +1,80 @@
+import { ReplacementsFetcher } from '../models/replacementsFetcher';
+import FetcherMetadata from '../models/fetcherMetadata';
+import TestUtilities from '../../tests/util';
+import Config from '../managers/config';
+import fs from 'fs';
+
+export default class FetcherLoader
+{
+	public static async loadFetcher(fetcherName: string): Promise<[ReplacementsFetcher, FetcherMetadata]>
+	{
+		const fetcher = await this.loadFetcherScript(fetcherName);
+		const metadata = this.loadMetadata(fetcherName);
+		return [fetcher, metadata];
+	}
+
+	private static loadFetcherScript(fetcherName: string): Promise<ReplacementsFetcher>
+	{
+		return new Promise((resolve, reject) =>
+		{
+			// While running bot it doesn't work with ts files anymore
+			// Hoverer, Jest don't create js files and still operates on ts
+			const path = `../fetchers/${fetcherName}/fetcher.${TestUtilities.isRunningInTest() ? 'ts' : 'js'}`;
+
+			import(path)
+				.then((fetcherClass) =>
+				{
+					let fetcher: ReplacementsFetcher;
+					if(!this.isFetcher(fetcherClass))
+					{
+						return Promise.reject(new Error(`"${fetcherName}" is not a ReplacementsFetcher`));
+					}
+
+					try
+					{
+						fetcher = new fetcherClass.default();
+					}
+					catch
+					{
+						reject(new Error('Fetcher thrown error inside constructor'));
+						return;
+					}
+
+					if(fetcher.initialize == null)
+					{
+						resolve(fetcher);
+					}
+					else
+					{
+						fetcher.initialize(Config.get('fetcher').config)
+							.then(() => resolve(fetcher))
+							.catch((error: Error) => reject(new Error(`Fetcher initialization error (${error})`)));
+					}
+
+				})
+				.catch((error) =>
+				{
+					reject(new Error(`Failed to import fetcher "${fetcherName}" (${error})`));
+				});
+		});
+	}
+
+	private static loadMetadata(fetcherName: string): FetcherMetadata
+	{
+		const metadataPath = __dirname + `/../fetchers/${fetcherName}/metadata.json`;
+		if(fs.existsSync(metadataPath))
+		{
+			return new FetcherMetadata(fs.readFileSync(metadataPath).toString(), fetcherName);
+		}
+		else
+		{
+			return new FetcherMetadata('{}', fetcherName);
+		}
+	}
+
+	private static isFetcher(object: any): boolean
+	{
+		const testObject = new object.default();
+		return testObject.fetchReplacements != undefined;
+	}
+}
